f1946835254a51a303bac944b4535e8866487336
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-27 00:52:15 +0800

    ask user to input a word

--- indexnumber.py
+++ indexnumber.py
@@ -1,5 +1,5 @@
 # Ask user to enter a word
-
+user_string = input("Enter a word: ")
 # Print characters at even indices using a loop:
 
 # Alternatively, print characters at even indices using string slicing:
