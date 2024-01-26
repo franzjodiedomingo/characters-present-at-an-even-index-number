9fc3b1d5fc8b6af804b67aabaa3d2131ceb63dc7
Franzjodie Domingo <franzjodiedomingo@gmail.com>
2024-01-27 01:00:12 +0800

    final

--- indexnumber.py
+++ indexnumber.py
@@ -1,6 +1,8 @@
 # Ask user to enter a word
 user_string = input("Enter a word: ")
+print("Original String:", user_string)
 # Print characters at even indices using a loop:
+print("Printing only even index chars")
 for i in range(0, len(user_string), 2):
     print(user_string[i])
 # Alternatively, print characters at even indices using string slicing:
